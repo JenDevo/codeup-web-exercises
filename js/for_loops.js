5baca45269fe230a8a8da94d625ab14a4c9cc648
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-01-26 16:58:57 -0600

    For loops exercises completed

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,28 +1,39 @@
 "use strict";
-// function showMultiplicationTable(num){
-//     for (let i= 1; i <= 10; i++){
-//
-//         console.log(num * i);
-//     }
-//     return "Complete";
-// }
+//Exercise #2
+function showMultiplicationTable(num){
+    for (let i= 1; i <= 10; i++){
 
-
-//function randomTen(){
-//     for (let i =1; i <= 10; i++){
-//
-//         let random = Math.floor((Math.random() * 200) + 20);
-//         if(random % 2 === 0){
-//             console.log(random + ' is even!');
-//         } else {
-//             console.log(random + " is odd");
-//         }
-//     }
-  //  return "Complete!";
-//}
-
-for(var i = 0; i <10; i++){
-
-    console.log()
+        console.log(num * i);
+    }
+    return "Complete";
 }
+
+//Exercise #3
+function randomTen(){
+    for (let i =1; i <= 10; i++){
+
+        let random = Math.floor((Math.random() * 200) + 20);
+        if(random % 2 === 0){
+            console.log(random + ' is even!');
+        } else {
+            console.log(random + " is odd");
+        }
+    }
+   return "Complete!";
+}
+
+//Exercise #4
+for(let i = 1; i <10; i++){
+    let numbers = "";
+    for (let x = 0; x <i; x++){
+        numbers += i;
+    }
+    console.log(numbers)
+}
+
+//Exercise #5
+for (var i= 100; i >= 5; i -= 5){
+    console.log(i);
+}
+
 
