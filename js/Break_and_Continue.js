b404bc2d539694db4a93f254058e5a3a1a981bed
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-01-26 17:58:02 -0600

    Completed Break and Continue Exercises

---
+++ js/Break_and_Continue.js
@@ -0,0 +1,34 @@
+"use strict";
+let numPicked;
+
+while(true) {
+    numPicked = prompt("Enter an odd number between 1 and 50.");
+    if (numPicked % 2 !== 0 && numPicked >= 1 && numPicked <= 50) {
+        break;
+    }
+    alert('Whoops! Please enter a number that is both odd and between 1 and 50.');
+}
+    console.log("Number to skip is: " + numPicked);
+
+for (let i = 1; i <= 50; i++){
+    if(i % 2 !==0){
+        if(i === Number(numPicked)){
+            console.log("Whoops! Skipping " + i);
+            continue;
+        }
+        console.log('Here is an odd number: ' + i);
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
