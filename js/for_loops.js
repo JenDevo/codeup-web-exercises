5455f808dd7f03bde50def4af074e89e35c86e76
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-01-26 16:44:37 -0600

    For loops...still working

---
+++ js/for_loops.js
@@ -0,0 +1,28 @@
+"use strict";
+// function showMultiplicationTable(num){
+//     for (let i= 1; i <= 10; i++){
+//
+//         console.log(num * i);
+//     }
+//     return "Complete";
+// }
+
+
+//function randomTen(){
+//     for (let i =1; i <= 10; i++){
+//
+//         let random = Math.floor((Math.random() * 200) + 20);
+//         if(random % 2 === 0){
+//             console.log(random + ' is even!');
+//         } else {
+//             console.log(random + " is odd");
+//         }
+//     }
+  //  return "Complete!";
+//}
+
+for(var i = 0; i <10; i++){
+
+    console.log()
+}
+
