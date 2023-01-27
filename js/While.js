f0ebcffcaa2a9a9e303dfa546ebfb559d5da8aca
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-01-26 23:03:32 -0600

    Still working on do_while loop exercise

--- js/While.js
+++ js/While.js
@@ -1,6 +1,34 @@
 "use strict";
-let i = 2;
-while (i <= 65536){
-    console.log(i);
-    i*=2;
-}
+//While Loop
+// let i = 2;
+// while (i <= 65536){
+//     console.log(i);
+//     i*=2;
+// }
+
+//Do-While Loop
+//generate random number between 50 and 100
+//generate random number between 1 and 5
+//create do while loop to log to console amt of cones sold to each person.
+
+// This is how you get a random number between 50 and 100
+
+do{
+    var allCones = Math.floor(Math.random() * 50) + 50;
+    var customerPurchase = Math.floor(Math.random() * 5) + 1;
+    console.log("The ice cream seller must sell a total of " + allCones + " cones today.");
+    // This expression will generate a random number between 1 and 5
+
+
+    if(customerPurchase < allCones){
+        console.log(customerPurchase + " sold");
+        continue;
+    } else if (customerPurchase > allCones){
+        console.log("Oh no! I do not have enough cones to sell you " + customerPurchase + "cones. I only have " + allCones + "left.");
+        continue;
+    } else if (customerPurchase === allCones){
+        console.log("Yay! I sold them all!");
+        break;
+    }
+    ++allCones;
+} while (allCones > 100);
