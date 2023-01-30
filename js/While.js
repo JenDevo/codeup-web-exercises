49c7c04d854b205ff2812086b37dca433d47f015
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-01-30 13:55:57 -0600

    A little close to do while solution

--- js/While.js
+++ js/While.js
@@ -11,9 +11,8 @@
 //generate random number between 1 and 5
 //create do while loop to log to console amt of cones sold to each person.
 
-// This is how you get a random number between 50 and 100
-
 do{
+    // This is how you get a random number between 50 and 100
     var allCones = Math.floor(Math.random() * 50) + 50;
     // This expression will generate a random number between 1 and 5
     var customerPurchase = Math.floor(Math.random() * 5) + 1;
@@ -27,7 +26,6 @@ do{
         continue;
     } else if (customerPurchase === allCones){
         console.log("Yay! I sold them all!");
-        break;
     }
     ++allCones;
-} while (allCones > 50);
+} while (allCones <= customerPurchase);
