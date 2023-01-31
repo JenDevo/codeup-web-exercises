06a9b34daf850f3fbae87f09a444ec75b2b98d3e
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-01-31 12:06:53 -0600

    Completed iterating arrays

--- js/iterating.js
+++ js/iterating.js
@@ -6,29 +6,40 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-
+let names = ['Amy', 'Tehya', 'Reese', 'Easton'];
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
+    console.log(names.length); // 4
 
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
 
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
+    for (let i = 0; i < names.length; i++){
+        console.log("Name at index " + i + " is " + names[i]);
+    }
 
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+    names.forEach(function(name, i = names.length){
+        console.log("Name at index " + i + " is " + name + "!");
+    })
 
     /**
      * TODO:
@@ -43,5 +54,20 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+  var numbers = ([1, 2, 3, 4, 5]);
+  function first(x){
+      return console.log(x[0]);
+  }
+
+  function second(x){
+      return console.log(x[1]);
+  }
+  function last(x){
+      return console.log(x[4]);
+    }
+
+  first(numbers);
+  second(numbers);
+  last(numbers);
 
 })();
