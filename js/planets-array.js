7415164a340eb2d17219638a0929e034669ed211
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-02-02 09:05:58 -0600

    Completed planets array exercises, starting planets string exercises

--- js/planets-array.js
+++ js/planets-array.js
@@ -20,21 +20,36 @@
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
     console.log(planets);
+    console.log(planets.unshift('The Sun'));
 
+    console.log('-----------------')
     console.log('Adding "Pluto" to the end of the planets array.');
     console.log(planets);
+    console.log(planets.push('Pluto'));
 
+    console.log('-----------------')
     console.log('Removing "The Sun" from the beginning of the planets array.');
     console.log(planets);
+    console.log(planets.shift('The Sun'));
 
+    console.log('-----------------')
     console.log('Removing "Pluto" from the end of the planets array.');
     console.log(planets);
+    console.log(planets.pop('Pluto'));
 
+    console.log('-----------------')
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    let index = planets.indexOf('Earth');
+    console.log('The index of Earth is: ' + index);
 
+    console.log('-----------------')
     console.log("Reversing the order of the planets array.");
     console.log(planets);
+    let reversePlan = planets.reverse();
+    console.log("The planets in reverse: " + reversePlan);
 
+    console.log('-----------------')
     console.log("Sorting the planets array.");
     console.log(planets);
+    console.log(planets.sort());
 })();
