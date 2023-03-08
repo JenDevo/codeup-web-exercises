2620957d9f296b0a4857ac3aedf67dd9e531dd06
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-03-08 15:04:21 -0600

    Still working...getting there

---
+++ js/jquery_faq.js
@@ -0,0 +1,26 @@
+"use strict";
+
+$("dd").addClass('invisible');
+
+
+    $('.invisible').css('visibility', 'hidden');
+
+
+    $('#toggle-link').click(function (e) {
+
+
+        $('dd').toggleClass('invisible')
+    });
+
+// CONTENT STYLING
+$('dl')
+    .css('padding', '1em')
+    .css('margin', '1em');
+
+$('dd')
+    .css('padding', '1em')
+
+$('dt')
+    .css('font-weight', 'bold');
+
+
