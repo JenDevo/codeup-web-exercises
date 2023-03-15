93e2a050a7ac18dbde516729fad1049ddd5b5e51
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-03-15 15:57:26 -0500

    Mostly complete with functionality and styling...how to fade forecast in and out

--- js/weather_map.js
+++ js/weather_map.js
@@ -3,7 +3,7 @@
 function geoCodeBuilder(searchString) {
 
     // Mapbox code goes below not weather key
-
+    // Code below is for using search bar to render data
     geocode(searchString, mapKey).then(function (results) {
         // BELOW: Calls back previous marker to move when searched.
         let myOptionsObj = {
@@ -16,19 +16,19 @@ function geoCodeBuilder(searchString) {
         // Get information from open weather API
         $.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${results[1]}&lon=${results[0]}&appid=${weatherKey}&units=imperial`).done(function (data) {
 
-            let html = "<div class='row d-flex justify-content-between'>";
+            let html = "<div class='row d-flex justify-content-around'>";
 
             for (let i = 0; i <= 39; i += 8) {
 
-                html += `<div class="card-decor col-2"><div class="date-header" ><p>${data.list[i].dt_txt}</p></div>`
+                html += `<div class="card-decor col-md-3"><div class="date-header" ><p>${data.list[i].dt_txt}</p></div>`
 
                 html += `<div class="d-flex justify-content-center" >`
 
-                html += `<h6 class="" >High: ${data.list[i].main.temp_max}&deg / Low: ${data.list[i].main.temp_min}&deg</h6>`;
+                html += `<h6 class="pt-2" >${data.list[i].main.temp_max}&deg / ${data.list[i].main.temp_min}&deg</h6>`;
 
                 html += `</div>`
 
-                html += `<div class="d-flex justify-content-center" >`
+                html += `<div class="d-flex justify-content-center hiLoImg">`
 
                 html += `<img src="http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png" class="align-content-center" >`
 
@@ -70,6 +70,7 @@ const marker = new mapboxgl.Marker({
 marker.setLngLat([-98.4946, 29.4252]);
 marker.addTo(map);
 
+// Code below is for using marker to render data
 
 marker.on('dragend', function (e) {
     let lonLat = e.target._lngLat;
@@ -80,7 +81,7 @@ marker.on('dragend', function (e) {
 
             let myOptionsObj = {
                 center: results,
-                zoom: 11
+                zoom: 13
             };
             map.flyTo(myOptionsObj);
             marker.setLngLat(results)
@@ -95,24 +96,23 @@ marker.on('dragend', function (e) {
 
                     html += `<div class="d-flex justify-content-center" >`
 
-                    html += `<h6 class="" >High: ${data.list[i].main.temp_max}&deg / Low: ${data.list[i].main.temp_min}&deg</h6>`;
+                    html += `<h6 class="pt-2" >${data.list[i].main.temp_max}&deg / ${data.list[i].main.temp_min}&deg</h6>`;
 
                     html += `</div>`
 
-                    html += `<div class="d-flex justify-content-center" >`
+                    html += `<div class="d-flex justify-content-center hiLoImg">`
 
                     html += `<img src="http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png" class="align-content-center" >`
 
                     html += `</div>`
 
-                    html += `<span>Description: <strong>${data.list[i].weather[0].description}</strong></span>`;
+                    html += `<div class="border" >Description: <strong>${data.list[i].weather[0].description}</strong></div>`;
 
+                    html += `<div class="border" >Humidity: <strong>${data.list[i].main.humidity} %</strong></div>`;
 
-                    html += `<span>Humidity: <strong>${data.list[i].main.humidity} %</strong></span>`;
+                    html += `<div class="border" >Wind: <strong>${data.list[i].wind.speed} MPH</strong></div>`;
 
-                    html += `<span>Wind: <strong>${data.list[i].wind.speed} MPH</strong></span>`;
-
-                    html += `<span>Pressure: <strong>${data.list[i].main.pressure}</strong></span></div>`;
+                    html += `<div>Pressure: <strong>${data.list[i].main.pressure}</strong></div></div>`;
 
                 }
                 html += "</div>"
