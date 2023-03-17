85d0b3f4a1284734a2d9a4fa2b68e88be112e6fb
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-03-17 15:00:00 -0500

    Refactor with arrow functions

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -42,7 +42,7 @@ console.log(users);
 // #2
 // Using filter to create array of user objects with languages
 // had issues with this at first because I wasn't using .length to target the language array
-let moreThanThreeLangArray = users.filter(function (user) {
+let moreThanThreeLangArray = users.filter((user) => {
    return user.languages.length >= 3
 });
 
@@ -52,7 +52,7 @@ console.log(moreThanThreeLangArray);
 // Using map, create array of strings where each element is a users email address
 //[strings of user emails]
 
-let userEmails = users.map(function (user){
+let userEmails = users.map((user) => {
     return users.email = user.email.toString()
 });
 
@@ -72,7 +72,7 @@ console.log(userEmails);
 
 
 //Below: doesn't give average but think this is what was expected, average is in commented out code above
-let totalYears = users.reduce(function (years, user){
+let totalYears = users.reduce( (years, user) => {
     return years + user.yearsOfExperience;
 }, 0)
 
@@ -81,7 +81,7 @@ console.log(`The average years worked amounts to: ${totalYears} years`);
 //#5
 //using reduce, get the longest email from the list of users
 
-let longestEmail = users.reduce(function(longest, user){
+let longestEmail = users.reduce((longest, user) => {
     if (user.email.length > longest.length){
         return user.email;
     } else {
@@ -95,7 +95,7 @@ console.log(longestEmail);
 //Using reduce, get list of user's names in single string
 //Example: Your instructors are: name, name, name, name
 
-let instructorList = users.reduce(function (stringAccum, person){
-    return stringAccum + person.name.toUpperCase() + "|";
-}, `List of Instructors:`)
+let instructorList = users.reduce((stringAccum, person) => {
+    return stringAccum + person.name.toUpperCase() + " | ";
+}, `List of Instructors: `)
 console.log(instructorList);
