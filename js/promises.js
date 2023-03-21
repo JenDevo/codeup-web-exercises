450a9ff24e29ef966004d5b1a8016796e52c3151
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-03-20 23:02:16 -0500

    Comlpeted promises exercise...want to work on bonus still

--- js/promises.js
+++ js/promises.js
@@ -5,7 +5,19 @@ function lastCommitDate(userName) {
 fetch(`https://api.github.com/users/` + userName + `/events/public`, {headers: {'Authorization': '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'}})
     .then(resolve => resolve.json())
     .then(data => {console.log(data[0].created_at)})
-
 }
 
 lastCommitDate("jendevo")
+
+
+//Create own promise
+// Still want to work on getting milliseconds in resolve
+
+function wait(num) {
+    return new Promise(resolve => setTimeout((num) => resolve(num), num))
+
+}
+
+wait(2000).then(() => console.log(`You will see this after 2 seconds`))
+wait(5000).then(() => console.log(`You will see this after 5 seconds`))
+wait(10000).then(() => console.log(`You will see this after 10 seconds`))
